1b0fe1e5a1b76c13174dc30ed2007c1b41bc8d4d
rafalander <rafa.lander@hotmail.com>
2025-09-22 11:54:36 -0300

    feature(TASK-3): Monta models de user

--- src/app/Models/User.php
+++ src/app/Models/User.php
@@ -2,15 +2,15 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable, HasRoles;
 
     /**
      * The attributes that are mass assignable.
@@ -21,6 +21,10 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'telefone',
+        'cpf',
+        'apartamento_id',
+        'status',
     ];
 
     /**
@@ -44,5 +48,18 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    /**
+     * Relationships
+     */
+    public function apartamento()
+    {
+        return $this->belongsTo(Apartamento::class);
+    }
+
+    public function encomendas()
+    {
+        return $this->hasMany(Encomenda::class, 'morador_id');
     }
 }
