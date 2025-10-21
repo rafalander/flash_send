aea9d02f2170a62130ef2bba6628b8eb4f8546f3
Rafael Lander Santos <rafa.lander@hotmail.com>
2025-10-21 18:10:03 -0300

    Revert "[WIP] Create CRUD interface for managing orders"

--- src/app/Models/Encomenda.php
+++ src/app/Models/Encomenda.php
@@ -6,20 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Encomenda extends Model
 {
-    protected $fillable = [
-        'descricao',
-        'data_recebimento',
-        'origem',
-        'codigo_rastreamento',
-        'morador_id',
-        'retirada',
-    ];
-
-    protected $casts = [
-        'data_recebimento' => 'date',
-        'retirada' => 'boolean',
-    ];
-
     public function morador() {
         return $this->belongsTo(Morador::class);
     }
