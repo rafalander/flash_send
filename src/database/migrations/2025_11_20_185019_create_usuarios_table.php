69eee5195e68c162f2fe5028ad83d443c0f8a235
rafalander <rafa.lander@hotmail.com>
2025-11-20 19:04:08 -0300

    feat(#167): Cria model, controller e migration de usuarios

---
+++ src/database/migrations/2025_11_20_185019_create_usuarios_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('usuarios', function (Blueprint $table) {
+            $table->id();
+            $table->string('nome');
+            $table->string('email');
+            $table->string('senha');
+            $table->string('telefone');
+            $table->string('cpf');
+            $table->foreignId('morador_id')->constrained('moradores')->onDelete('cascade');
+            $table->string('type');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('usuarios');
+    }
+};
